4bab21afb86ed9009e3a04909a6b7106aca51d85
YogeshJain96 <54388005+YogeshJain96@users.noreply.github.com>
2019-12-27 00:13:46 +0530

    printing search result in console  only

--- EcomWebApp_PHP/js/searchProduct.js
+++ EcomWebApp_PHP/js/searchProduct.js
@@ -7,16 +7,16 @@ function searchProducts() {
     let input = document.getElementById('searchBar').value; 
     input=input.toLowerCase(); 
     console.log("Searching: "+input);
-    let x = document.getElementsByClassName('filterDiv'); 
+    //let x = document.getElementsByClassName('filterDiv'); 
+    let x = document.getElementsByClassName('productName'); 
       
     for (i = 0; i < x.length; i++) {  
         if (!x[i].innerHTML.toLowerCase().includes(input)) { 
-           x[i].style.display="none";
+           // x[i].style.display="none";
         } 
         else { 
-           x[i].style.display="show";
-           var x=document.getElementsByClassName('productName');
-           console.log("Found: " + x[0].innerHTML);                  
+           // x[i].style.display="show";
+           console.log("Found: " + x[i].innerHTML);                  
         } 
     } 
-} 
+}
